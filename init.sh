c64b2bfc968e205271b65003407f0ee8d8d79c64
zloypur <zloypur@gmail.com>
2019-03-15 00:02:49 +0300

    Fix config

--- init.sh
+++ init.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx stop
-sudo /etc/init.d/nginx start
+sudo /etc/init.d/nginx restart
 # sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
 # sudo /etc/init.d/mysql start
