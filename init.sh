5758333575fe80a8f97882356d93dab615c806db
zloypur <zloypur@gmail.com>
2019-03-15 00:11:19 +0300

    Fix config

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 # sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
